34a8bcc1f53643b0159846a74909228ac2a3dbfc
fvahid <vahidmohsseni@yahoo.com>
2016-07-23 17:26:41 +0430

    builder is a module to build course and teacher and gardes

---
+++ builder.py
@@ -0,0 +1,35 @@
+import temp
+
+
+__author__ = "VahidMohsseni"
+
+
+grade_counter = 1
+grade_dic = {}
+
+
+def make_grade(grade_name):
+    global grade_counter
+    grade_dic[grade_name] = temp.Grades(grade_name, grade_counter)
+    grade_counter += 1
+
+
+course_counter = 1
+course_name_id_dic = {}
+course_dic = {}
+
+
+def make_course(course_name, hour, grade_name):
+    global course_counter
+    course_name_id_dic[course_counter] = temp.Course(course_name, hour, grade_dic[grade_name], course_counter)
+    course_counter += 1
+
+teacher_counter = 1
+teacher_name_id_dic = {}
+
+
+def make_teacher(name, title, course_list, days, hours):
+    global teacher_counter
+    teacher_name_id_dic[teacher_counter] = temp.Teacher(name, title, course_list, days, hours)
+    teacher_counter += 1
+
--- temp.py
+++ temp.py
@@ -1,21 +1,32 @@
-
-__autor__ = "moeb1376"
+__author__ = "moeb1376"
+__editor__ = "VahidMohsseni"
 
 
 class School:
-    def __init__(self, courses, schedule_hours):
-        self.courses = courses  # list of course
-        self.schedule_hours = schedule_hours  # example [[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2]]
+    def __init__(self, teachers_list, grades_list):
+        self.teachers = teachers_list
+        self.grades = grades_list
 
 
-class Teacher:
-    def __init__(self, course, working_days, working_hours, grade):
-        self.course = course
-        self.working_days = working_days
-        self.working_hours = working_hours
-        self.grade = grade
+class Grades:
+    def __init__(self, name, grade_id):
+        self.name = name
+        self.id = grade_id
+        self.courses_list = None
 
 
 class Course:
-    def __init__(self, name, hour, grade):
-        pass
+    def __init__(self, name, hour, grade, course_counter):
+        self.name = name
+        self.hour = hour
+        self.grade = grade
+        self.id = course_counter
+
+
+class Teacher:
+    def __init__(self, name, title, course_list, working_days, working_hours):
+        self.name = name
+        self.title = title  # Mr. or Mrs.
+        self.course = course_list
+        self.working_days = working_days
+        self.working_hours = working_hours
