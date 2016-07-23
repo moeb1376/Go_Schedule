1679774b64470db8f33aa81058ed9419d2d11e98
fvahid <vahidmohsseni@yahoo.com>
2016-07-23 15:58:09 +0430

    improve classes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Go_Schedule
-havij
+    :) :) :)
--- temp.py
+++ temp.py
@@ -1,15 +1,21 @@
-class school:
-    def __init__(self, courses, schedule_hours):
-        self.courses = courses #list of course
-        self.schedule_hours = schedule_hours #example [[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2]]
 
-class teacher:
-    def __init__(self,course,working_days,working_hours,grade):
+__autor__ = "moeb1376"
+
+
+class School:
+    def __init__(self, courses, schedule_hours):
+        self.courses = courses  # list of course
+        self.schedule_hours = schedule_hours  # example [[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2]]
+
+
+class Teacher:
+    def __init__(self, course, working_days, working_hours, grade):
         self.course = course
         self.working_days = working_days
         self.working_hours = working_hours
         self.grade = grade
 
-class course:
-    def __init__(self,name,hour,grade):
+
+class Course:
+    def __init__(self, name, hour, grade):
         pass
